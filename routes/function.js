66c597691745263df4fbd815d435e587809fa150
DmalTzi <Audyomg@gmail.com>
2023-08-19 08:57:12 +0700

    add

--- routes/function.js
+++ routes/function.js
@@ -1,6 +1,6 @@
 const Data = require("../model/Data")
 
-async function random_serial(){
+function random_serial(){
     console.log("i'm in random")
     let ran_str = ''
     for (let i = 0; i < 5; i++){
@@ -13,7 +13,7 @@ async function random_serial(){
     }).catch((err) => {
         console.log(err)
     });
-    return await (String(ran_str))
+    return (String(ran_str))
     
 }
 
