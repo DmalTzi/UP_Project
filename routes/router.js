8eb5e11e228cf725e12bbaf575e6db42a86f5313
DmalTzi <Audyomg@gmail.com>
2023-08-26 20:18:16 +0700

    init

--- routes/router.js
+++ routes/router.js
@@ -236,23 +236,4 @@ router.post("/update", (req,res)=>{
     res.redirect("/")
 })
 
-
-
-
-// router.post("/webhook", line.middleware(lineConfig), async (req,res)=>{
-//     try{
-//         const events = req.body.events
-//         console.log("event=>>>>>>",events)
-//         return events.length > 0 ? await events.map(item => handleEvent(item)) : res.status(200).send("ok")
-//     } catch (error){
-//         console.log(error)
-//         res.status(500).end()
-//     }
-// })
-
-// const handleEvent = async (event)=>{
-//     console.log(event)
-//     // return client.pushMessage(to="U8ceb07e1a18493975adbcbacf9b63368",{type:"text",text:"Hello World Test"})
-// }
-
 module.exports = router
